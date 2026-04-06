28afeb01362ffa29e33cc3f0388fec71419b9312
drewt0 <tttolca@gmail.com>
2026-04-06 08:50:59 -0700

    Created bubble object

--- first_p5_project/sketch.js
+++ first_p5_project/sketch.js
@@ -1,7 +1,31 @@
+let bubble;
+
 function setup() {
     createCanvas(400, 400);
+    bubble = new Bubble();
 }
 
 function draw() {
     background(0);
+    bubble.move();
+    bubble.show();
+}
+
+class Bubble {
+    constructor() {
+        this.x = 200;
+        this.y = 100;
+        this.size = 40;
+    }
+
+    move() {
+        this.x++;
+        this.y++;
+    }
+
+    show() {
+        noStroke();
+        fill(0, 255, 255);
+        ellipse(this.x, this.y, this.size)
+    }
 }
