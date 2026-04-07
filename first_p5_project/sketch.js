c952186edb38d849d6c37763b412edacc0777f3b
drewt0 <tttolca@gmail.com>
2026-04-07 07:30:59 -0400

    Fixed sticky wall glitch and refined splitting logic

--- first_p5_project/sketch.js
+++ first_p5_project/sketch.js
@@ -2,50 +2,54 @@ let bubbles = [];
 
 function setup() {
     createCanvas(innerWidth-18, innerHeight-20);
-    bubbles[0] = new Bubble(width/2, height/2, 2, 2, 250);
+    bubbles[0] = new Bubble(width/2, height/2, 2, 2, 1, 1, 250);
 }
 
 function draw() {
     background(255);
 
-    for (let bubble of bubbles) {
-        bubble.move();
+    for (let i = 0; i < bubbles.length; i++) {
+        let b = bubbles[i];
+        b.move();
 
-        if (bubble.bubbleHover()) {
-            bubble.a = 100;
+        if (b.bubbleHover()) {
+            b.a = 100;
         } else {
-            bubble.a = 200;
+            b.a = 200;
         }
 
-        for (let other of bubbles) {
-            bubble.bubbleCollision(other);
+        for (let j = i + 1; j < bubbles.length; j++) {
+            b.bubbleCollision(bubbles[j]);
         }
 
-        bubble.show();
+        b.show();
     }
 }
 
 function mouseClicked() {
     for (let i = bubbles.length-1; i >= 0; i--) {
-        if (bubbles[i].bubbleHover()) {
-            bubbles[i].dx = random(1, 5);
-            bubbles[i].dy = random(1,5);
-            bubbles[i].r = bubbles[i].r/1.6
-            bubbles.push(new Bubble(bubbles[i].x + bubbles[i].r/2*bubbles[i].dxMult, bubbles[i].y + bubbles[i].r/2*bubbles[i].dyMult, bubbles[i].dx * -bubbles[i].dxMult, bubbles[i].dy * -bubbles[i].dyMult, bubbles[i].r));
+        let bub = bubbles[i];
+        if (bub.bubbleHover()) {
+            bub.dx = random(1, 5);
+            bub.dy = random(1, 5);
+            bub.r /= 1.6
+            bub.x += bub.r/1.5*bub.dxMult;
+            bub.y += bub.r/1.5*bub.dyMult;
+            bubbles.push(new Bubble(bub.x + bub.r/1.5*(-bub.dxMult), bub.y + bub.r/1.5*(-bub.dyMult), bub.dx, bub.dy, -bub.dxMult, -bub.dyMult, bub.r));
         }
     }
 }
 
 class Bubble {
-    constructor(x, y, dx, dy, r) {
+    constructor(x, y, dx, dy, dxMult, dyMult, r) {
         this.x = x;
         this.y = y;
         this.dx = dx;
         this.dy = dy;
         this.r = r;
         this.a = 200;
-        this.dxMult = 1;
-        this.dyMult = 1;
+        this.dxMult = dxMult;
+        this.dyMult = dyMult;
     }
 
     bubbleHover() {
@@ -59,13 +63,16 @@ class Bubble {
 
     bubbleCollision(other) {
         let d = dist(this.x, this.y, other.x, other.y);
-        if (d < (this.r + other.r)/2) {
+        let minDist = (this.r + other.r)/2;
+        if (d < minDist) {
             this.dxMult *= -1;
             this.dyMult *= -1;
             other.dxMult *= -1;
             other.dyMult *= -1;
 
+            let overlap = minDist - d;
             let angle = atan2(other.y - this.y, other.x - this.x);
+
             this.x -= cos(angle) * 2;
             this.y -= sin(angle) * 2;
             other.x += cos(angle) * 2;
@@ -79,17 +86,19 @@ class Bubble {
 
         if (this.x > width - this.r/2) {
             this.dxMult = -1;
-            this.x = width - this.r/2;
-        } else if (this.x < 0 + this.r/2) {
+            // this.x = width - this.r/2;
+        } 
+        if (this.x < 0 + this.r/2) {
             this.dxMult = 1;
-            this.x = this.r/2
+            // this.x = this.r/2
         }
         if (this.y > height - this.r/2) {
             this.dyMult = -1;
-            this.y = height - this.r/2;
-        } else if (this.y < 0 + this.r/2) {
+            // this.y = height - this.r/2;
+        } 
+        if (this.y < 0 + this.r/2) {
             this.dyMult = 1;
-            this.y = this.r/2
+            // this.y = this.r/2
         }
             
     }
