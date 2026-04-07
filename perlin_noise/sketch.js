f659e3ac1533b98185253bdc818a574e6f1be5a9
drewt0 <tttolca@gmail.com>
2026-04-07 10:38:21 -0400

    added second dimension

--- perlin_noise/sketch.js
+++ perlin_noise/sketch.js
@@ -1,14 +1,33 @@
+let xOff = 0;
+let yOff = 1;
+
 function setup() {
     createCanvas(innerWidth, innerHeight);
-}
-
-function draw() {
     background(0);
     fill(255);
     noStroke();
 
-    for (let i = 30; i < width; i+=1) {
-        let h = map(noise(i/100), 0, 1, 0, height)
-        ellipse(i, h, 1);
+    for (let i = 0; i < 10000; i++) {
+        let w = map(noise(xOff), 0, 1, 0, width)
+        let h = map(noise(yOff), 0, 1, 0, height)
+        ellipse(w, h, 1);
+
+        xOff+=0.02;
+        yOff+=0.02;
     }
+}
+
+function draw() {
+    // background(0);
+    // fill(255);
+    // noStroke();
+    // if (xOff < 100) {
+    //     let w = map(noise(xOff), 0, 1, 0, width)
+    //     let h = map(noise(yOff), 0, 1, 0, height)
+    //     ellipse(w, h, 1);
+
+    //     xOff+=0.02;
+    //     yOff+=0.02;
+    // }
+
 }
