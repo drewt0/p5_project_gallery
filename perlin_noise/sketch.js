c30c7ecf0b8521db9e657f43264a1ee9ea02ea24
drewt0 <tttolca@gmail.com>
2026-04-07 11:05:40 -0400

    Added scrolling landscape

--- perlin_noise/sketch.js
+++ perlin_noise/sketch.js
@@ -1,33 +1,29 @@
-let xOff = 0;
-let yOff = 1;
+let inc = 0.01;
+let start = 0;
 
 function setup() {
     createCanvas(innerWidth, innerHeight);
     background(0);
     fill(255);
     noStroke();
-
-    for (let i = 0; i < 10000; i++) {
-        let w = map(noise(xOff), 0, 1, 0, width)
-        let h = map(noise(yOff), 0, 1, 0, height)
-        ellipse(w, h, 1);
-
-        xOff+=0.02;
-        yOff+=0.02;
-    }
 }
 
 function draw() {
-    // background(0);
-    // fill(255);
-    // noStroke();
-    // if (xOff < 100) {
-    //     let w = map(noise(xOff), 0, 1, 0, width)
-    //     let h = map(noise(yOff), 0, 1, 0, height)
-    //     ellipse(w, h, 1);
+    background(0);
+    stroke(255);
+    noFill();
 
-    //     xOff+=0.02;
-    //     yOff+=0.02;
-    // }
+    beginShape();
+    let xOff =  start;
+    for (let x = 0; x < width; x++) {
+        let y = noise(xOff) * height;
+        vertex(x, y);
+
+        xOff += inc;
+    }
+
+    start += inc;
+
+    endShape();
 
 }
