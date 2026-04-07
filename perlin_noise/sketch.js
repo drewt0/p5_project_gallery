5d6420657004183d1887bc42a04abeb2bd670695
drewt0 <tttolca@gmail.com>
2026-04-07 10:37:24 -0400

    perlin noise first attempt

--- perlin_noise/sketch.js
+++ perlin_noise/sketch.js
@@ -4,4 +4,11 @@ function setup() {
 
 function draw() {
     background(0);
+    fill(255);
+    noStroke();
+
+    for (let i = 30; i < width; i+=1) {
+        let h = map(noise(i/100), 0, 1, 0, height)
+        ellipse(i, h, 1);
+    }
 }
