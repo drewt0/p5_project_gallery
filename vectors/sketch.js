ebec16ebb28b9c83253f49ef59f211ee802fe605
drewt0 <tttolca@gmail.com>
2026-04-08 09:36:45 -0400

    random walker using vectors and noise

--- vectors/sketch.js
+++ vectors/sketch.js
@@ -1,7 +1,26 @@
+let pos;
+let incx, incy;
+
 function setup() {
     createCanvas(innerWidth, innerHeight);
+
+    incx = 1;
+    incy = 2;
+
+    pos = createVector(width/2, height/2);
+    background(0);
 }
 
 function draw() {
-    background(0);
+    // background(0);
+
+    stroke(255);
+    strokeWeight(2);
+    point(pos.x, pos.y);
+
+    pos.x += map(noise(incx), 0, 1, -1, 1);
+    pos.y += map(noise(incy), 0, 1, -1, 1);
+
+    incx += 0.01;
+    incy += 0.01;
 }
