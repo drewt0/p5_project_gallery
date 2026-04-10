9677ed0dad95b873396ad88bdfc545062823abff
drewt0 <tttolca@gmail.com>
2026-04-09 20:17:13 -0400

    added firework deletion to improve frame rate

--- vectors/sketch.js
+++ vectors/sketch.js
@@ -9,15 +9,22 @@ function setup() {
 function draw() {
     background(0);
 
-    for (let f of fireworks) {
+    for (let i = fireworks.length; i >= 0; i--) {
+        let f = fireworks[i];
+
+        if (!f) continue;
         f.show();
+
+        if (f.particles.length == 0) {
+            fireworks.splice(i, 1);
+        }
     }
 
     mouse = createVector(mouseX, mouseY)
 }
 
 function mouseClicked() {
-    fireworks.push(new Firework(mouse.x, mouse.y));
+    fireworks.push(new Firework(mouseX, mouseY));
 }
 
 class Firework {
@@ -25,21 +32,35 @@ class Firework {
         this.x = x;
         this.y = y;
         this.particles = [];
-        this.a = 100;
+        this.alpha = [];
+        this.timer = 0;
 
         for (let i = 0; i < 100; i++) {
             this.particles.push(p5.Vector.random2D());
             this.particles[i].mult(random(30, 90));
+
+            this.alpha.push(random(90, 200));
         }
     }
 
 
     show() {
-        stroke(255, 255, 255, this.a);
-        for (let p of this.particles) { 
+        for (let i = this.particles.length; i >= 0; i--) {
+            let p = this.particles[i];
+            let a = this.alpha[i];
+
+            if (!p) continue;
+
+            this.alpha[i] -= 2;
+            if (a < 1) {
+                this.particles.splice(i, 1);
+                this.alpha.splice(i, 1);
+                continue;
+            }
+
+            stroke(255, 255, 255, a);
             line(this.x, this.y, this.x + p.x, this.y + p.y);
             p.mult(1.02);
         }
-        this.a--;
     }
 }
