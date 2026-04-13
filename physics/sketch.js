bd63757acd1fb820e755d35ada440e37ea221d00
drewt0 <tttolca@gmail.com>
2026-04-12 23:35:26 -0400

    bouncing ball with gravity

--- physics/sketch.js
+++ physics/sketch.js
@@ -1,8 +1,37 @@
+let x, y;
+let xSpeed, ySpeed;
+let gravity = 0.6;
+let radius = 25;
+
 function setup() {
-    createanvas(innerWidth, innerHeight);
-    background(0);
+	createCanvas(600, 400);
+	x = width / 2;
+	y = height / 2;
+	xSpeed = 5;
+	ySpeed = 2;
 }
 
 function draw() {
-    
+	background(220);
+
+	fill(50, 150, 255);
+	noStroke();
+	ellipse(x, y, radius * 2);
+
+	ySpeed += gravity;
+	x += xSpeed;
+	y += ySpeed;
+
+	if (y + radius > height) {
+		y = height - radius;
+		ySpeed *= -0.8;
+	}
+
+	if (x + radius > width || x - radius < 0) {
+		xSpeed *= -1;
+	}
+
+	if (y - radius < 0) {
+		ySpeed *= -1;
+	}
 }
