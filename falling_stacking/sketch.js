738cdf4063dd533b8e355e37b9009154288eceb5
drewt0 <tttolca@gmail.com>
2026-04-18 11:49:45 -0400

    created array to hold pixels + visual grid

--- falling_stacking/sketch.js
+++ falling_stacking/sketch.js
@@ -1,7 +1,39 @@
+function make2DArray(cols, rows) {
+    let arr = new Array(cols);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+    }
+    return arr;
+}
+
+let grid;
+let w = 10;
+let cols, rows;
+
 function setup() {
-    createCanvas(innerWidth, innerHeight);
+    createCanvas(500, 500);
+    cols = width / w;
+    rows = height / w;
+    grid = make2DArray(cols, rows);
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = 0;
+        }
+    }
 }
 
 function draw() {
     background(0);
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            stroke(255);
+            fill(grid[i][j] * 255);
+            let x = i * w;
+            let y = j * w;
+            square(x, y, w);
+        }
+    }
 }
+
